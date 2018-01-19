001078982e55c4dd48193f80901b045f782bbc94
Micah Wells <>
2018-01-19 14:00:33 -0600

    Added messagig to app includig a message componenets

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -18,7 +18,8 @@ export class HeroesComponent implements OnInit {
   heroes = [];
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero): void {
